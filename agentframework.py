98e8d6de9fa6cf88f16d4a22abf876c83391b981
tburgess97 <98338298+tburgess97@users.noreply.github.com>
2022-04-04 18:47:41 +0100

    Assessment Commit

--- agentframework.py
+++ agentframework.py
@@ -1,77 +1,224 @@
+'''
+Agent class
+Contains agents attributes and their behaviours
+
+As ran through model.py
+
+Version 1
+'''
+
+# Packages
+
+# Import the packages required for the class
+
+# Standard library packages
 import random
 
 
+# Agent class
+
+# Initiate an Agent class for the agents and define their behaviours as 
+# functions
+
 class Agent:
+
     
-# initiate new instance of the agent class and assign random coords; __init__
-# method sets initial state of object (an agent)
-
-
     def __init__(self, environment, agents, y = None, x = None):
-        self._y = random.randint(0,99)
-        self._x = random.randint(0,99)
+        '''
+        Function to initiate the agent; reads in the environment and sets y and
+        x coordinates, assigns random y and x coordinates if none are read
+        and sets the agents store 
+
+        Parameters
+        ----------
+        environment :
+            THE ENVIRONMENT AS A 2D LIST FROM model.py.
+        agents :
+            THE AGENT LIST FROM model.py.
+        y : 
+            THE Y COORDINATE OF THE AGENT FROM model.py. The default is None 
+            if none are passed.
+        x : 
+            THE X COORDINATE OF THE AGENT FROM model.py. The default is None 
+            if none are passed.
+
+        Returns
+        -------
+        None.
+
+        '''
+        self._y = random.randint(0,299)
+        self._x = random.randint(0,299)
         self.environment = environment
         self.store = 0
         self.agents = agents
         
         if (x == None):
-            self._x = random.randint(0,99)
+            self._x = random.randint(0,299)
         else:
             self._x = x
             
         if (y == None):
-            self._y = random.randint(0,99)
+            self._y = random.randint(0,299)
         else:
             self._y = y
         
         
     def __str__(self):
+        '''
+        Function that defines its own values as a string
+
+        Returns
+        -------
+        Its own values as a string.
+
+        '''
         return " x " + str(self._x) + " y " + str(self._y)
         
+    
 # get and set functions for coords
     def getx(self):
         return self._x
+        '''
+        A get function for the x coordinate 
+        
+        Returns
+        -------
+        The read x coordinate.
+        '''
+    
     
     def setx(self, value):
         self._x = value
+        '''
+         A set function for the x coordinate 
+        
+        Returns
+        -------
+        The set x coordinate.
+        '''
+    
         
     def gety(self):
         return self._y
+        '''
+        A get function for the y coordinate 
+        
+        Returns
+        -------
+        The read y coordinate.
+        '''
        
+        
     def sety(self, value):
         self._y = value
+        '''
+        A set function for the y coordinate 
+        
+        Returns
+        -------
+        The set y coordinate.
+        '''
         
     x = property(getx, setx, "I'm the 'x' property")
     y = property(gety, sety, "I'm the 'y' property")
     
     
-# move instance by 1 step (one x step, one y step) and boundary affect
+# move instance by 1 step (one x step, one y step)
     def move(self):
+        '''
+        A function to move the agent by one step in a random direction and 
+        apply a fence boundary affect to the agent
+
+        Returns
+        -------
+        The new y coordinate.
+        THe new x coordinate.
+
+        '''
         if random.random() < 0.5:
-            self._y = (self._y + 1) % 100
+            self._y = (self._y + 1) 
         else:
-            self._y = (self._y - 1) % 100
+            self._y = (self._y - 1) 
             
         if random.random() < 0.5:
-            self._x = (self._x + 1) % 100
+            self._x = (self._x + 1)
         else:
-            self._x = (self._x - 1) % 100
+            self._x = (self._x - 1)
     
+        # Fence boundary affect to keep the agent within the environment limits
+        if self._y < 0:
+            self._y = 0
+        if self._y > 299:
+            self._y = 299
+        
+        if self._x <0:
+            self._x = 0
+        if self._x > 299:
+            self._x = 299
+
+
     def eat(self):
+        '''
+        A function that defines the agent's eat behaviour; to remove 10 units
+        from the environment at the coordinates it has
+
+        Returns
+        -------
+        The new environment value.
+        The new agent store value.
+
+        '''
         if self.environment[self._y][self._x] > 10:
             self.environment[self._y][self._x] -= 10
             self.store += 10
+
             
+    def distance_between(self, agent):
+        '''
+        A function that defines the agent's eucalidian distance between itself
+        and other agents
+
+        Parameters
+        ----------
+        agent :
+            THE AGENT LIST FROM model.py.
+
+        Returns
+        -------
+        The eucalidan distance value. 
+
+        '''
+        return (((self._x - agent._x)**2) + ((self._y - agent._y)**2))**0.5
    
+    
     def share_with_neighbours (self, neighbourhood):
+        '''
+        A function to make the agent share its store with the closest agent if
+        the distance is less than the neighbourhood parameter assigned at the 
+        command line
+
+        Parameters
+        ----------
+        neighbourhood : 
+            NEIGHBOURHOOD PARAMETER VALUE FROM model.py.
+
+        Returns
+        -------
+        The agents new store value.
+
+        '''
         for agent in self.agents:
             dist = self.distance_between(agent)
             if dist <= neighbourhood:
                 sum = self.store + agent.store
-                ave = sum /2
+                ave = sum / 2
                 self.store = ave
                 agent.store = ave
-                #print("sharing" + str(dist) + " " + str(ave))# check print 
+                # Uncomment to check that agents are sharing with their 
+                # neighbours 
+                '''
+                print("sharing" + str(dist) + " " + str(ave))
+                '''
                 
-    def distance_between(self, agent):
-        return (((self._x - agent._x)**2) + ((self._y - agent._y)**2))**0.5
+    
---
+++ model.py
@@ -0,0 +1,276 @@
+'''
+Agent-based Model 
+
+Version 1
+
+Author: gy21t2b, 201466497, University of Leeds
+
+As produced for GEOG5990: Assignment 1
+
+To be run in the command prompt as per the README documentation
+'''
+
+
+# Packages and modules
+
+# Import the packages and modules required for the model
+
+# Standard library packages
+import random 
+import operator 
+import tkinter 
+import csv 
+import argparse 
+
+# Third party packages
+import requests
+import bs4
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.backends.backend_tkagg
+import matplotlib.pyplot
+import matplotlib.animation
+
+# Local application modules
+import agentframework
+
+print("Libraries and modules imported")
+
+
+# Command line run set up
+
+# Model parameters set up to be input at the command line (with default 
+#                                                         parameters used when 
+#                                                         none are specified)
+# Command line argument source code adapted from:
+    # https://docs.python.org/3/library/argparse.html
+parser = argparse.ArgumentParser(description = 'Agent-based model')
+parser.add_argument("num_of_agents", type = int, nargs ='?', const = 10, 
+                    default = 10, help = "This sets the number of agents as \
+                        an integer")
+parser.add_argument("num_of_iterations", nargs = '?', type = int, const = 100,
+                    default = 100, help = "This sets the number of iterations \
+                        as an integer")
+parser.add_argument("neighbourhood", nargs = '?', type = int, const = 20,
+                    default = 20, help = "This sets the neighbourhood as an \
+                        integer")
+
+args = parser.parse_args()
+num_of_agents = args.num_of_agents
+num_of_iterations = args.num_of_iterations
+neighbourhood = args.neighbourhood
+# Confirm the parameters used
+print("Parameters used: Agents =",num_of_agents,", Iterations =",
+      num_of_iterations,", Neighbourhood =",neighbourhood)
+
+
+# Environment                  
+                                                             
+# Read in the environment as raster data
+f = open('in.txt', newline='')
+reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+
+# Create an environment container
+environment = []
+
+# Append the raster data to the environment as a 2D list 
+for row in reader: 
+    rowlist = []
+    for value in row:
+        rowlist.append(value)
+    environment.append(rowlist)
+f.close()
+# Uncomment to check the raster environment
+'''
+print(environment)
+'''
+
+
+# Agents
+
+# Create agents container
+agents = []
+
+# Scrape starting y and x coordinates for agents from webpage:
+    # http://www.geog.leeds.ac.uk/courses/computing/practicals/python/agent-framework/part9/data.html
+# BeautifulSoup (BS4) source code adapted from:
+    # https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+r = requests.get('http://www.geog.leeds.ac.uk/courses/computing/practicals/python/agent-framework/part9/data.html')
+content = r.text
+soup = bs4.BeautifulSoup(content, 'html.parser')
+td_ys = soup.find_all(attrs={"class" : "y"})
+td_xs = soup.find_all(attrs={"class" : "x"})
+# Uncomment to check the coordinates obtained
+'''
+print(td_ys)
+print(td_xs)
+'''
+
+# Create agents and assign starting coordinates as scraped above
+for i in range(num_of_agents):
+    y = int(td_ys[i].text)
+    x = int(td_xs[i].text)
+    agents.append(agentframework.Agent(environment, agents, y, x))
+
+#  Starting coordinates assigned to agents
+print("Starting agent coordinates:")
+for agent in agents:
+    print(agent)
+    
+# Figure set up
+
+# Set the matplotlib figure and axis size
+fig = matplotlib.pyplot.figure(figsize=(7, 7))
+ax = fig.add_axes([0, 0, 1, 1])
+
+
+# Run model
+
+carry_on = True
+def update(frame_number):
+    '''
+    Function to define what is undertaken during each iteration:
+     Clears the figure.
+     The order that the agents run through their functions are randomly 
+     shuffled. 
+     Each agent runs through move, eat and share with neighbour functions.
+     A stopping condition is defined where all agents stores meet the store 
+     limit.
+     Updates the environment and the plotted agents as an animation.
+    
+    Parameters
+    ----------
+    frame_number :
+        THE NUMBER OF ITERATIONS THE UPDATE FUNCTION IS RAN THROUGH.
+    
+    Returns 
+    -------
+    Prints confirmation of the met stopping condition.
+    '''
+    fig.clear() 
+    global carry_on
+    # Check that the model is running through the iterations.
+    '''
+    print("Model updating")
+    '''
+    
+    # Uncomment the for statements above and below random.shuffle to check that 
+    # the order of which the agents are ran through their functions are 
+    # randomly shuffled
+    '''
+    for a in agents:
+        print("Before shuffle:", a.x, a.y)
+    '''
+    random.shuffle(agents)
+    '''
+    for a in agents:
+        print("After shuffle:", a.x, a.y) # test shuffle 
+    '''
+    
+    for i in range(num_of_agents):
+        agents[i].move()
+        agents[i].eat()
+        agents[i].share_with_neighbours(neighbourhood)
+   
+    full_count = 0
+    store_limit = 500
+    for i in range(num_of_agents):
+        if agents[i].store > store_limit:
+            full_count = full_count + 1
+            # Uncomment to check that each agents stores are filling
+            '''
+            print("Agent store full")
+            '''
+            
+    if full_count == num_of_agents:
+        carry_on = False
+        print("Stopping condition: All agents stores full")
+        
+    matplotlib.pyplot.imshow(environment)
+    for i in range(num_of_agents):
+        matplotlib.pyplot.scatter(agents[i].x, agents[i].y, c='red')
+
+
+def gen_function():
+    ''' 
+    Function to continue through the model iterations as per the 
+    update function until stopping conditions are met:
+     a) Maximum number of iterations as set at command line met. 
+     b) Agents store limit met.
+    
+    Returns
+    -------
+    Prints confirmation that the model has met one of the stopping conditions.
+    Prints the iteration number that the model's stopping condition is met at.
+    Prints the final coordinates of each agent.
+    Prints the final stores of each agent.
+    '''
+    a = 0
+    global carry_on
+    while (a < num_of_iterations) & (carry_on):
+        yield a
+        a = a + 1
+    print("MODEL END")
+    print("Iteration number at stopping condition:", a)
+    #  Final coordinates assigned to agents
+    print("Final agent coordinates:")
+    for agent in agents:
+        print(agent)
+    print("Final agent stores:")
+    for agent in agents:
+        print(agent.store)
+
+
+def run():
+    '''
+    Function to run the model as an animation
+
+    Returns
+    -------
+    Prints confirmation that the model has started to run through the update
+    function.
+
+    '''
+    global animation 
+    animation = matplotlib.animation.FuncAnimation(fig, update,
+                                                   frames=gen_function,
+                                                   repeat=False, interval = 1)
+    print("MODEL START")
+    canvas.draw()
+    
+
+def close():
+    '''
+    Function to close the model
+
+    Returns
+    -------
+    Prints confirmation that the model GUI has been closed.
+
+    '''
+    root.quit()
+    root.destroy()
+    print("MODEL CLOSED")
+
+
+# GUI set up
+
+root = tkinter.Tk()
+root.protocol("WM_DELETE_WINDOW", close)
+root.wm_title("Agent-based Model")
+
+canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=root)
+canvas._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+menu_bar = tkinter.Menu(root)
+root.config(menu=menu_bar)
+model_menu = tkinter.Menu(menu_bar)
+menu_bar.add_cascade(label="Model", menu=model_menu)
+model_menu.add_command(label="Run model", command=run)
+model_menu.add_command(label="Close model", command=close)
+
+tkinter.mainloop()
+     
+
+#List of dependancies (i.e. what packages and versions required to install to
+# run) (pip install package)
